e9e60614301266c938bb7016dd0e4dae3ae4fdd2
Niels Rewijk <niels.rewijk@student.rocva.nl>
2021-02-09 12:09:04 +0100

    gemaakt

--- 02-Javascript/taak01-Herhaling/js/script.js
+++ 02-Javascript/taak01-Herhaling/js/script.js
@@ -1,2 +1,17 @@
 let strNaam;
 let boolNaamKlopt;
+strNaam = prompt("wat is je naam?");
+boolNaamKlopt = confirm("is " + strNaam +" je naam?");
+if (boolNaamKlopt == true){
+    confirm("welkom " + strNaam);
+    }
+else{
+    strNaam = prompt("wat is je Naam?");
+    boolNaamKlopt = confirm("is " + strNaam +" je naam?");
+        if (boolNaamKlopt == true){
+            confirm("welkom " + strNaam);
+            }
+        else{
+            alert("Welkom mens zonder naam")
+            }     
+    }
