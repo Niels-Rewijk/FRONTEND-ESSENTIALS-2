dc68191ca8777698a68e301e63d7d3a4d71eddc6
Niels Rewijk <niels.rewijk@student.rocva.nl>
2021-02-10 10:55:24 +0100

    gameaakt

--- 02-Javascript/taak03-Functions-Return/js/script.js
+++ 02-Javascript/taak03-Functions-Return/js/script.js
@@ -1,5 +1,14 @@
-function geefRandomGetalTerug(){                
-    
+function geefRandomGetalTerug(randomGetal){                
+    return randomGetal = Math.random() *9 + 1; 
 }
 
 console.log(geefRandomGetalTerug());    
+console.log(geefRandomGetalTerug());
+console.log(geefRandomGetalTerug());
+console.log(geefRandomGetalTerug());
+console.log(geefRandomGetalTerug());
+console.log(geefRandomGetalTerug());
+console.log(geefRandomGetalTerug());
+console.log(geefRandomGetalTerug());
+console.log(geefRandomGetalTerug());
+console.log(geefRandomGetalTerug());
